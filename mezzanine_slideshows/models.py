c8f6b9bf7cc02c081e401e4ad9c49741879d6ec2
Philip Southwell <phil@zoothink.com>
2015-05-10 20:55:00 +0930

    Remove model fields "title" and "active". Add a __str__ method.

--- mezzanine_slideshows/models.py
+++ mezzanine_slideshows/models.py
@@ -5,12 +5,12 @@ from mezzanine.galleries.models import Gallery
 
 
 class Slideshow(models.Model):
-    title = models.CharField(max_length=30,
-                             help_text="The title of your slideshow")
     page = models.OneToOneField(RichTextPage)
     galleries = models.ManyToManyField(Gallery)
-    active = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = "Slideshow"
         verbose_name_plural = "Slideshows"
+
+    def __str__(self):
+        return self.title
