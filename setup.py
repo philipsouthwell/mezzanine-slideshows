e68b31e4f48fdf6c06516362f4389c207f2d65ce
Philip Southwell <phil@zoothink.com>
2015-05-11 20:48:03 +0930

    Change setup for next release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -9,11 +9,11 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='mezzanine-slideshows',
-    version='0.2.2',
+    version='0.2.3',
     packages=['mezzanine_slideshows'],
     include_package_data=True,
     license='BSD License',
-    description='A simple mezzanine app which allows the placement of a mezzanine Gallery within any other mezzanine Page as a slideshow ',
+    description='A simple mezzanine app which allows the placement of Mezzanine Galleries within other Mezzanine Pages as slideshows',
     long_description=README,
     url='https://github.com/philipsouthwell/mezzanine-slideshows',
     author='Philip Southwell',
