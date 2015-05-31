5c78d5cb1ee4548da1c84ed68bc567912fa64335
Philip Southwell <phil@zoothink.com>
2015-05-31 14:01:20 +0930

    Prepare for packaging

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='mezzanine-slideshows',
-    version='0.3.1',
+    version='0.3.2',
     packages=['mezzanine_slideshows'],
     include_package_data=True,
     license='BSD License',
